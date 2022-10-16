38765656c03f6f0de22da6df52c52a3336e8b0b6
elifhelincarboga <elifhelincarboga@gmail.com>
2022-10-16 21:30:45 +0300

    add: js file

---
+++ js/index.js
@@ -0,0 +1,77 @@
+function toogleTask () {
+  this.classList.toggle("checked");
+}
+
+function newElement () {
+  const input = document.getElementById('task')
+  const text = input.value
+
+  if (input.value === '') {
+    const myToast = $('#liveToast.error')
+    myToast.toast('show')
+    return
+  }
+
+  const element = createElement(text)
+  let list = document.getElementById('list')
+  list.appendChild(element)
+
+  const myToast = $('#liveToast.success')
+  myToast.toast('show')
+
+  $(element).on('click', toogleTask)
+
+  var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+  if (!storedTasks) {
+    let arr = []
+    arr.push(text)
+    storedTasks = arr
+  } else {
+    storedTasks.push(text)
+  }
+  localStorage.setItem("tasks", JSON.stringify(storedTasks));
+}
+
+function createElement (value) {
+  const element = document.createElement('li')
+  element.innerHTML = value
+  return element
+}
+
+function removeTask () {
+  let list = document.getElementById('list')
+  var text = this.parentElement.childNodes[0].data
+
+  var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+  var index = storedTasks.findIndex((i) => i === text)
+  storedTasks.splice(index, 1)
+  localStorage.setItem("tasks", JSON.stringify(storedTasks));
+
+  this.parentElement.remove()
+}
+
+$(document).ready(function() {
+  let list = document.getElementById('list')
+
+  var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  if (!storedTasks) {
+    return
+  }
+
+  for (let i = 0; i < storedTasks.length; i++) {
+    const element = createElement(storedTasks[i])
+    const span = document.createElement('span')
+    span.style.float = 'right'
+    span.innerHTML = 'x'
+    element.appendChild(span)
+    $(span).on('click', removeTask)
+    list.appendChild(element)
+    $(element).on('click', toogleTask)
+  }
+});
+
+
+
+
+
